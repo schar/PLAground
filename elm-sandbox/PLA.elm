482649f11137092c699cdb249b7114d972840308
Dylan Bumford <dbumford@gmail.com>
2015-03-09 23:26:44 -0400

    switch to combinators from parser library

--- elm-sandbox/PLA.elm
+++ elm-sandbox/PLA.elm
@@ -1,140 +1,77 @@
-
+import Parser (..)
+import Parser.Char (lower, parenthesized)
+import Parser.Number (digit)
 import Graphics.Input.Field as Field
-import Signal (..)
+import Signal as Sig
 import Graphics.Element (..)
 import Text (plainText)
 import List (concatMap)
-import String (fromChar, filter, toList, fromList)
+import String (fromChar, filter, toList, fromList, cons)
 
 
-main : Signal Element
-main = map scene (subscribe content)
--- main = putStrLn . gimme $ parse "  (~  (Ex((~e( x) ) & o ( x  ) ) ))"
-
-content : Channel Field.Content
+-- open a reactive channel to text contents
+content : Sig.Channel Field.Content
 content =
   let startText = "(Ex (e(x) & o(x)))"
       startChan = Field.Content startText (Field.Selection 0 0 Field.Forward)
-  in channel startChan
+  in Sig.channel startChan
 
-
+-- handler to process the reactive field contents at any given moment
 scene : Field.Content -> Element
 scene fieldContent =
-  let defaultText = "(Ex (e(x) & o(x)) )"
+  let clean = filter (\x -> x /= ' ')
+      -- default text for the field when empty
+      defText = "enter formula here"
+      -- clean and attempt to parse the field contents
+      resultText =
+        case parse (form <* end) (clean fieldContent.string) of
+          Err x -> "oops"
+          Ok x  -> x
+  -- stack two boxes on top of one another; the first a text input box hooked
+  -- up to our reactive content channel, the second a box with the parse
+  -- results
   in flow down
-     [ Field.field Field.defaultStyle (send content) defaultText fieldContent
-     , plainText <| gimme <| parse fieldContent.string
+     [ Field.field Field.defaultStyle (Sig.send content) defText fieldContent
+     , plainText resultText
      ]
 
--- monadic bits
-type alias M a = String -> List (a, String)
+-- process the channel reactively (send current text contents through handler)
+main : Signal Element
+main = Sig.map scene (Sig.subscribe content)
 
 
-ret : a -> M a
-ret a = \s -> [(a, s)]
-
-bind : M a -> (a -> M b) -> M b
-m `bind` f = \s -> concatMap (\(a, s') -> f a s') <| m s
-
--- zero and plus
-zero : M a
-zero x = []
-
-plus : M a -> M a -> M a
-m `plus` n = \s -> m s ++ n s
-
--- item
-item : M Char
-item s =
-  let sss = toList s
-  in case sss of
-    [] -> [(' ', "")]
-    (x::xs) -> [(x, fromList xs)]
-
--- filtering
-filt : M a -> (a -> Bool) -> M a
-m `filt` p = m `bind` \a -> 
-          if p a
-          then ret a
-          else zero
-
--- literals
-lit : Char -> M Char
-lit c = item `filt` \a -> a == c
+-- Grammar
+------------------------------------------------------------------------------
 
 -- variables
-var : M String
-var =
-  let isAsciiLower c = c >= 'a' && c <= 'z'
-  in (item `filt` \a -> isAsciiLower a) `bind` \a -> ret (fromChar a)
+var : Parser String
+var = map fromChar lower
 
 -- pronouns
-pro : M String
-pro =
-  let isDigit c = c >= '0' && c <= '9'
-  in lit 'p' `bind` \a -> 
-       (item `filt` \a -> isDigit a) `bind` \b ->
-         ret <| fromChar a ++ fromChar b
+pro : Parser String
+pro = map cons (symbol 'p') `and` map toString digit
 
 -- terms
-term : M String
-term = pro `plus` var
+term : Parser String
+term = pro `or` var
 
--- predicates
-onePlace : M Char
-onePlace = lit 'e' `plus` lit 'o'
+showpred = \x y -> "(Pred " ++ fromChar x ++ " " ++ y ++ ")"
+showneg = \x -> "(Neg " ++ x ++ ")"
+showquant = \x y -> "(" ++ x ++ " " ++ y ++ ")"
+showconj = \x y -> "(Conj " ++ x ++ " " ++ y ++ ")"
 
--- conjunction
-conj : M Char
-conj = lit '&'
-
--- negation
-neg : M Char
-neg = lit '~'
- 
--- quantifiers
-quant : M String
-quant = lit 'E' `bind` \a -> 
-          var `bind` \b ->
-            ret <| fromChar a ++ b
+pars : Parser String -> Parser String
+pars = parenthesized
 
 -- formulae
-form : M String
+form : Parser String
+-- form = pred `or` quant `or` neg `or` conj
 form =
-  (onePlace `bind` \a ->
-    lit '(' `bind` \_ ->
-      term `bind` \b -> 
-        lit ')' `bind` \_ ->
-          ret <| "(Pred " ++ fromChar a ++ " " ++ b ++ ")")
-  `plus` 
-  (lit '(' `bind` \_ ->
-    quant `bind` \a -> 
-      form `bind` \b ->
-        lit ')' `bind` \_ ->
-          ret <| "(" ++ a ++ " " ++ b ++ ")")
-  `plus`
-  (lit '(' `bind` \_ ->
-    neg `bind` \_ ->
-      form `bind` \a -> 
-         lit ')' `bind` \_ ->
-           ret <| "(Neg " ++ a ++ ")")
-  `plus`
-  (lit '(' `bind` \_ ->
-    form `bind` \a ->
-       conj `bind` \_ ->
-         form `bind` \b -> 
-           lit ')' `bind` \_ ->
-             ret <| "(Conj " ++ a ++ " " ++ b ++ ")")
+  let self  = recursively <| \() -> form
+      pred  = map showpred (symbol 'e' `or` symbol 'o') `and` pars term
+      neg   = pars <| symbol '~' *> map showneg self
+      quant = pars <| map showquant (map cons (symbol 'E') `and` var) `and` self
+      conj  = pars <| map showconj self <* symbol '&' `and` self
+  in pred `or` neg `or` quant `or` conj
 
-clean : String -> String
-clean = filter (\x -> x /= ' ')
 
-parse : M String
-parse = form << clean
-
--- gimme
-gimme : List (String, String) -> String
-gimme l =
-  case l of
-    [] -> "oops"
-    ((x,y)::xs) -> x
